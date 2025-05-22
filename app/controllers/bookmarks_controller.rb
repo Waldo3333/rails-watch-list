8ddee34889170a5026a1b325b50fcf7975706229
charlie gardere <charlie.gardere@gmail.com>
2025-05-22 17:31:37 +0200

    fontionnel css sur la show

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: :create
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@bookmark.list)
+  end
+   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+   end
+
+
+  private
+
+  def bookmark_params
+     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+end
