637e52dede98318f9a885dac1cff16ddb6805895
charlie gardere <charlie.gardere@gmail.com>
2025-05-23 11:39:46 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ index = 0
 Bookmark.destroy_all
 Movie.destroy_all
 
-30.times do
+20.times do
   Movie.create!(title: "#{data["results"][index]["title"]}", overview: "#{data["results"][index]["overview"]}", poster_url: "https://image.tmdb.org/t/p/original/#{data["results"][index]["poster_path"]}", rating: "#{data["results"][index]["vote_average"]}")
   index = index + 1
   puts "#{index} movie created"
